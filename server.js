33745390c12d6ab5e9f0f51b7448e8d44a4ffab9
krausyd <krausyd@hotmail.com>
2022-01-20 20:04:45 -0800

    (∩◕-◕ )⊃━✿✿✿✿ Add new post api to create new animal

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const { query } = require('express');
 const express = require('express');
 const { animals } = require('./data/animals');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     console.log('GET /api/animals', req.query);
@@ -21,6 +29,45 @@ app.get('/api/animals/:id', (req, res) => {
         res.sendStatus(404);
     }
 });
+
+app.post('/api/animals', (req, res) => {
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // req.body is where our incoming content will be
+        console.log('POST /api/animals', req.body);
+        req.body.id = animals.length.toString();
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+
+const validateAnimal = (animal) => {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+};
+
+const createNewAnimal = (body, animalsArray) => {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+};
 
 const findById = (id, animalsArray) => {
     const result = animalsArray.filter(animal => animal.id === id)[0];
